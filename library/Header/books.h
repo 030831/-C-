574ef1ad3974871500d18e71df5d1388cbecdc34
030831 <101914089+030831@users.noreply.github.com>
2022-07-31 16:18:55 +0900

    Add files via upload

---
+++ library/Header/books.h
@@ -0,0 +1,19 @@
+//h에는 구조체 , 변수 , 함수원형
+
+#pragma once
+
+#ifndef _BOOKS_H_
+#define _BOOKS_H_
+
+
+
+print_books();  //현재 도서관의 책 목록 출력
+borrow_books(book_input); // 책 빌리기 함수
+Return_books(); //책 반납 함수
+Return_books_txt(); //반납후 txt파일 수정 함수
+
+
+#endif
+
+
+// 수정
