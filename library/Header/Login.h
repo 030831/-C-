f1f16cab07eccd7f1524653ec02c54ec2b98fb4f
030831 <101914089+030831@users.noreply.github.com>
2022-07-31 21:21:32 +0900

    Add files via upload

---
+++ library/Header/Login.h
@@ -0,0 +1,19 @@
+//h에는 구조체 , 변수 , 함수원형
+
+#pragma once
+
+#ifndef _LOGIN_H_
+#define _LOGIN_H_
+
+struct Loginadmin //로그인 아이디 , 비밀번호 구조체 선언
+{
+	char id[6];
+	char password[5];
+	char Login_id[20];
+	char Login_password[20];
+
+};
+
+int setLogin(); // 로그인 함수
+
+#endif
